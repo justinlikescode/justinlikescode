22a63fe50ae391a5499a1d1ad9037581924233a8
thatONEjustin <2763180+thatONEjustin@users.noreply.github.com>
2026-09-16 13:53:03 -0500

    localhost fallback

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import alpinejs from "@astrojs/alpinejs";
 
 import react from "@astrojs/react";
 
-const SITE_URL = process.env.NODE == "dev" ? "https://localhost:4321" : process.env.SITE_URL;
+const SITE_URL = process.env.MODE == "dev" ? "https://localhost:4321" : process.env.SITE_URL;
 // https://astro.build/config
 export default defineConfig({
     output: "static",
